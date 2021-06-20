0dec71126027c7df0dae758ec9f30c06c6c5861c
root <root@shpakovsky.ru>
2021-06-20 22:22:52 +0200

    typo fix: rename squirrelmail container

--- daily.sh
+++ daily.sh
@@ -3,6 +3,6 @@ ls dovecot.cont/data/mail/ | sed 's/.*/& ok/' >postfix.cont/data/conf/users.txt
 for user in `ls dovecot.cont/data/mail/`; do
 	grep -q '### auto-trash ###' dovecot.cont/data/sieve/$user.sieve || continue
 	sed -i '/### auto-trash ###/q' dovecot.cont/data/sieve/$user.sieve
-	cut -d'|' -f4 sq.cont/data/squirrelmail/$user.abook | sed 's/.*/"&",/' >>dovecot.cont/data/sieve/$user.sieve
+	cut -d'|' -f4 squirrelmail.cont/data/squirrelmail/$user.abook | sed 's/.*/"&",/' >>dovecot.cont/data/sieve/$user.sieve
 	echo '"x"]{ fileinto "Trash"; }' >>dovecot.cont/data/sieve/$user.sieve
 done
