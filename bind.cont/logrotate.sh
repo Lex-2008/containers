a5ecb278e7ca8d7a5ecc9eef70b9ebd0a4e247ec
root <root@shpakovsky.ru>
2021-10-17 00:14:43 +0200

    fixes to make logrotate work

--- bind.cont/logrotate.sh
+++ bind.cont/logrotate.sh
@@ -2,8 +2,8 @@ test -f "${0%/*}"/logrotate-pre.sh && . "${0%/*}"/logrotate-pre.sh
 
 cd "${0%/*}"/data/logs
 DATE="$(date -u +'%Y%m%d-%H%M%S')"
-mv mail.log mail.log.$DATE
+mv bind.log bind.log.$DATE
 
-"${0%/*}"/reload.sh
+sh ../../reload.sh
 
-gzip mail.log.$DATE
+gzip bind.log.$DATE
