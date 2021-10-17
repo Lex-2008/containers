e8a4a3645c39c1e9bedfbba1f12fbbfd2f4994e0
root <root@shpakovsky.ru>
2021-10-17 16:58:46 +0200

    use native lua loop instead of get_rcpt(n) which seems to be broken

--- dkim.cont/data/conf/setup.lua
+++ dkim.cont/data/conf/setup.lua
@@ -1,11 +1,14 @@
 fromdom = odkim.get_fromdomain(ctx)
 odkim.log(ctx, "mail from domain: " .. fromdom)
 if fromdom == "shpakovsky.ru" then
-	nrcpts = odkim.rcpt_count(ctx)
-	for n = 1, nrcpts do
-		rcpt = odkim.get_rcpt(ctx, n - 1)
+	allrcpts = odkim.get_rcptarray(ctx)
+	-- odkim.log(ctx, "all rcpts: " .. table.concat(allrcpts, ", "))
+	for n, rcpt in ipairs(allrcpts) do
+		-- odkim.log(ctx, "rcpt " .. n .. " is: " .. rcpt)
+		-- if string.find(rcpt, "port25", 1, true) ~= nil then
 		if string.find(rcpt, "mailop.org", 1, true) ~= nil then
 			odkim.log(ctx, "receiver would benefit from changing From header: " .. rcpt)
+			odkim.log(ctx, odkim.get_header(ctx, "From", 0))
 			odkim.replace_header(ctx, "From", 0, "Alexey Shpakovsky via mailop <mailop@mailop.org>")
 			-- return nil
 		end
