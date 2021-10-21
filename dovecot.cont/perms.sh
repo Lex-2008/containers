ff325df0cc95de42ee827b90df8bca7a17064a36
root <root@shpakovsky.ru>
2021-10-21 23:27:00 +0200

    cleanup dovecot perms

--- dovecot.cont/perms.sh
+++ dovecot.cont/perms.sh
@@ -1,4 +1,6 @@
 cd "${0%/*}"
-mkdir -p data/logs
+mkdir -p data/logs data/sieve data/mail
+test -f data/conf/passwd.txt || touch data/conf/passwd.txt
 chown 90:101 data/conf/passwd.txt
 chmod u=r,go= data/conf/passwd.txt
+chown -R 1000:1000 data/sieve data/mail
