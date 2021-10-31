e8fce75c0905510eee0adbe5e35cfe510a25def9
root <root@shpakovsky.ru>
2021-10-31 18:39:52 +0100

    use internal connection to logshow server - follow-up to e9712a3

--- postfix.cont/logrotate-pre-example.sh
+++ postfix.cont/logrotate-pre-example.sh
@@ -6,7 +6,7 @@ cd /containers/nginx.cont/data/public/alexey.shpakovsky.ru/en/
 savelog() {
 	from="$1"
 	to="${2-$from}"
-	wget -q http://alexey.shpakovsky.ru/logshow/$from.sh -O $to.htm.new
+	wget -q 127.0.0.1:8003/$from.sh -O $to.htm.new
 	cat $to.htm >>$to.htm.new
 	chown --reference=$to.htm $to.htm.new
 	mv $to.htm.new $to.htm
