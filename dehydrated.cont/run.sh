82c8cb9745f90cfb1429c5ccf220a9923a33544c
root <root@shpakovsky.ru>
2021-11-14 16:23:32 +0100

    Do not require terminal to run dehydrated
    In order to run it non-interactively via cron.

--- dehydrated.cont/run.sh
+++ dehydrated.cont/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd "${0%/*}"
-docker run --rm -it -v $PWD/data:/data -v $PWD/../bind.cont/data/key.conf:/key.conf --link bind:bind --name=dehydrated dehydrated git pull
-docker run --rm -it -v $PWD/data:/data -v $PWD/../bind.cont/data/key.conf:/key.conf --link bind:bind --name=dehydrated dehydrated ./dehydrated -c
+docker run --rm -i -v $PWD/data:/data -v $PWD/../bind.cont/data/key.conf:/key.conf --link bind:bind --name=dehydrated dehydrated git pull
+docker run --rm -i -v $PWD/data:/data -v $PWD/../bind.cont/data/key.conf:/key.conf --link bind:bind --name=dehydrated dehydrated ./dehydrated -c
 test -f data/deploy_cert || exit 0
 cp data/certs/shpakovsky.ru/fullchain.pem data/certs/shpakovsky.ru/privkey.pem ../nginx.cont/data/cert/
 sh ../nginx.cont/reload.sh
