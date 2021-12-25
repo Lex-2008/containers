2d68d9f906e587b9bf67bfa530eef82acde45945
root <root@shpakovsky.ru>
2021-12-25 15:31:26 +0100

    Remove unnecessary speed optimizations

--- calc.cont/build/nums.c
+++ calc.cont/build/nums.c
@@ -39,45 +39,29 @@ int main(int argc, char *argv[])
                 return 3;
         }
 
-        char replacements[10][9] = {
-                {'1', '2', '3', '4', '5', '6', '7', '8', '9'}, //0
-                {'0', '2', '3', '4', '5', '6', '7', '8', '9'}, //1
-                {'0', '1', '3', '4', '5', '6', '7', '8', '9'}, //2
-                {'0', '1', '2', '4', '5', '6', '7', '8', '9'}, //3
-                {'0', '1', '2', '3', '5', '6', '7', '8', '9'}, //4
-                {'0', '1', '2', '3', '4', '6', '7', '8', '9'}, //5
-                {'0', '1', '2', '3', '4', '5', '7', '8', '9'}, //6
-                {'0', '1', '2', '3', '4', '5', '6', '8', '9'}, //7
-                {'0', '1', '2', '3', '4', '5', '6', '7', '9'}, //8
-                {'0', '1', '2', '3', '4', '5', '6', '7', '8'}};//9
-
-        for( size_t i=0; i<str_len; i++ ) {
-                char base_char = str[i];
-                unsigned short r = base_char - '0';
-                if( r >= 10 ){
-                        printf("got bad r=%d on char [%c] (number %ld)\n", r, base_char, i);
-                        return 2;
-                }
-                // Dear Compiler, please optimise this
-                base[0*str_len1 + i] = replacements[r][0];
-                base[1*str_len1 + i] = replacements[r][1];
-                base[2*str_len1 + i] = replacements[r][2];
-                base[3*str_len1 + i] = replacements[r][3];
-                base[4*str_len1 + i] = replacements[r][4];
-                base[5*str_len1 + i] = replacements[r][5];
-                base[6*str_len1 + i] = replacements[r][6];
-                base[7*str_len1 + i] = replacements[r][7];
-                base[8*str_len1 + i] = replacements[r][8];
-                fwrite(base,str_len+1,9,stdout);
-                base[0*str_len1 + i] = base_char;
-                base[1*str_len1 + i] = base_char;
-                base[2*str_len1 + i] = base_char;
-                base[3*str_len1 + i] = base_char;
-                base[4*str_len1 + i] = base_char;
-                base[5*str_len1 + i] = base_char;
-                base[6*str_len1 + i] = base_char;
-                base[7*str_len1 + i] = base_char;
-                base[8*str_len1 + i] = base_char;
-        }
+	for( size_t i=0; i<str_len; i++ ) {
+		char base_char = str[i];
+		unsigned short r = base_char - '0';
+		if( r >= 10 ){
+			printf("got bad r=%d on char [%c] (number %ld)\n", r, base_char, i);
+			return 2;
+		}
+		char c='0';
+		for(unsigned short j=0; j<9; j++ ) {
+			// j is index, 0..8 inclusive
+			// c is character, '0'..'9' inclusive, excluding base_char
+			// base_char is the character we're looking at
+			// r is its numberic value
+			if(c==base_char)
+				// skip this char,
+				c++;
+			base[j*str_len1 + i] = c;
+			c++;
+		}
+		fwrite(base,str_len1,9,stdout);
+		for( unsigned short j=0; j<9; j++ ) {
+			base[j*str_len1 + i] = base_char;
+		}
+	}
 
 }
