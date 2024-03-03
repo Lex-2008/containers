f11e68cd49b6f9a8e18bd1d56b08958778bc68c8
Alexey Shpakovsky <alexey@shpakovsky.ru>
2024-03-03 16:12:41 +0100

    compress stalker data for backup purposes

--- stalker.cont/run-main.sh
+++ stalker.cont/run-main.sh
@@ -3,3 +3,5 @@
 cd "${0%/*}"
 OUTDIR="/containers/gemini.cont/data/hosts/stalker.shpakovsky.ru"
 docker run --rm -v $PWD/data:/data -v $OUTDIR:/ext stalker /data/main.sh main >"$OUTDIR/log.txt" 2>&1
+cd /containers/stalker.cont/data
+tar czf data.tar.gz data
